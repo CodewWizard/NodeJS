a259b1c92168978db439856bf84c7deb98a6f4b6
Codewizard <madihamujawar410@gmail.com>
2023-04-29 15:38:59 +0530

    node

---
+++ index.js
@@ -0,0 +1,30 @@
+// console.log("madiha");
+
+// const os = require('os');
+// console.log(os.arch());
+
+// const freeMem = os.freemem();
+// console.log(`${freeMem/1024/1024/1024}`);
+
+// validator module----
+
+// var validator = require('validator');
+
+// const res = validator.isEmail('madiha@gmail.com');
+
+// console.log(res);
+
+
+
+// module wrapper-------
+// format of module wrapper function ----
+// (function(exports, require, module, __filename, __dirname){
+
+//});
+
+(function (a){
+    console.log('madiha codewizard '+a);
+    var a;
+}("maddy"));
+console.log(__dirname);
+console.log(__filename);
