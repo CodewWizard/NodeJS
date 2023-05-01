d7d31173da0c32be61023f14632ce2bf42152697
Codewizard <madihamujawar410@gmail.com>
2023-05-01 12:54:24 +0530

    node

--- Http-Server/index.js
+++ Http-Server/index.js
@@ -1,7 +1,9 @@
 const http = require('http');
 
 const server = http.createServer((request, response)=>{
-    response.end('hello from port no 8000');
+    console.log(request.url);
+    response.end('<h1>hello from port no 8000</h1>');
+    
 });
 
 server.listen(8000, "127.0.0.1", ()=>{
