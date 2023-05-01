6a7cbc23e9bb3534536a5edcdbb36a7449adc5df
Codewizard <madihamujawar410@gmail.com>
2023-05-01 13:07:15 +0530

    json

---
+++ Json/index.js
@@ -0,0 +1,4 @@
+// Json stands for javascript object notation
+// json is lightweight
+// format for storing and transporting data
+// json is often used when data is sent from a server to a webpage
